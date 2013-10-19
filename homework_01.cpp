81f4a02912a815d23c7533fea0a4c4d4ee4e66c1
Lubomir <Lubomir@mars.com>
2013-10-20 03:58:13 +0400

    HW_01, all except "change array to vector<>"

--- homework_01.cpp
+++ homework_01.cpp
@@ -1,19 +1,21 @@
-/*  Change to C++ io
-*   change to one line comments
-*   change defines of constants to const
-*   change array to vector<>
-*   inline any short function
-*/
+// Change to C++ io
+// change to one line comments
+// change defines of constants to const
+// change array to vector<>
+// inline any short function
 
-#include <stdio.h>
-#define N 40
-void sum(int*p, int n, int d[])
+#include <iostream>
+using namespace std;
+const int N = 40;
+
+inline void sum(int*p, int n, int d[])
 {
   int i;
   *p = 0;
   for(i = 0; i < n; ++i)
      *p = *p + d[i];
 }
+
 int main()
 {
    int i;
@@ -24,6 +26,6 @@ int main()
       data[i] = i;
 
     sum(&accum, N, data);
-    printf("sum is %d\n", accum);
+    cout << "sum is " << accum << endl;
 return 0;
 }
