bad71ae7842d58ffe557c46e087cd97675e27e90
Lubomir <Lubomir@mars.com>
2013-10-20 04:54:50 +0400

    HW_01 add comments

--- homework_01.cpp
+++ homework_01.cpp
@@ -1,30 +1,35 @@
+// Homework 1: Convert a C program to C++
 // change to C++ io
 // change to one line comments
 // change defines of constants to const
 // change array to vector<>
 // inline any short function
+ 
+// C original code is shown for reference at the end-line comments.
 
-#include <iostream>
+#include <iostream>     // #include <stdio.h>
 #include <vector>
 
 using namespace std;
-const int N = 40;
+const int Number_of_elements = 40;       // #define N 40
 
-inline void sum(int& p, int n, vector<int> d)
+// Sum the elements of a vector (array)
+// argument "accum" is passed by reference
+inline void sum(int& accum, int Number_of_elements, vector<int> data)
 {
-    p = 0;
-    for(int i = 0; i < n; ++i)
-        p = p + d[i];
+    accum = 0;
+    for(int i = 0; i < Number_of_elements; ++i)
+        accum = accum + data[i];
 }
 
 int main()
 {
     int accum = 0;
-    vector<int> data(N);
-    for(int i = 0; i < N; ++i)
+    vector<int> data(Number_of_elements);    // int data[N];
+    for(int i = 0; i < Number_of_elements; ++i)
         data[i] = i;
 
-    sum(accum, N, data);
-    cout << "sum is " << accum << endl;
+    sum(accum, Number_of_elements, data);
+    cout << "sum is " << accum << endl;  // printf("sum is %d\n", accum);
     return 0;
 }
